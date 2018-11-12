536b7063fcdc2aea954dceb529db350c1865ce21
mabenunez <mariabnhernandez@gmail.com>
2018-11-12 08:55:23 -0300

    all info from github api is set

---
+++ index.js
@@ -0,0 +1,19 @@
+$.ajax(
+    "https://api.github.com/users/mabenunez"
+  ).done(function(data) {
+    $('.photo').append(`<img src='${data.avatar_url}' />`)
+    $('#website').html(`https://"${data.blog}"`)
+});
+
+$.ajax(
+    "https://api.github.com/users/mabenunez/repos"
+  ).done(function(data) {
+    console.log(data)
+    for (var i = 0; i < data.length; i++) {
+        var name = `<p>Name: <span>${data[i].name}</span></p>`;
+        var url = `<p>url: <span>${data[i].html_url}</span></p>`;
+        var date = `<p>Last update: <span>${data[i].created_at}</span></p>`;
+        var star = `<p>${data[i].stargazers_count}<i class="fas fa-star"></i></p>`;
+        $('.repo-container').append(`<div class='repo'>${star}${name}${url}${date}</div>`);
+    }
+});
